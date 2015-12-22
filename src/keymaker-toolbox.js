d1ab1e19cbb770ce6480687abff093553df63378
Hemslo Wang <hemslo.wang@gmail.com>
2015-12-22 21:22:06 +0800

    Add basic makeKey method

--- src/keymaker-toolbox.js
+++ src/keymaker-toolbox.js
@@ -1,6 +1,45 @@
+import crypto from 'crypto';
+
+const Characters = {
+  lower: 'abcdefghijklmnopqrstuvwxyz'.split(''),
+  upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+  number: '0123456789'.split(''),
+  special: '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split('')
+};
+
 const keymakerToolbox = {
-  greet() {
-    return 'hello';
+  makeKey(password, salt, iterations=100000, keylen=32, lower=true, upper=true, number=true, special=true) {
+    let dk = crypto.pbkdf2Sync(password, salt, iterations, keylen, 'SHA256');
+    let hex = dk.toString('hex');
+
+    let types = [];
+    const allTypes = ['lower', 'upper', 'number', 'special'];
+    [lower, upper, number, special].forEach((e, i) => {
+      if (e) {
+        types.push(allTypes[i]);
+      }
+    });
+
+    if (types.length === 0 || keylen < types.length) {
+      return hex;
+    }
+
+    let decs = hex.match(/.{2}/g).map(i => parseInt(i, 16));
+    let characters = decs.map(dec => {
+      let type = types[dec % types.length];
+      return Characters[type][dec % Characters[type].length];
+    });
+
+    // ensure all types at least one
+    let indices = characters.map((e, i) => i);
+    let index = 0;
+    types.forEach(type => {
+      let i = decs[index] % indices.length;
+      index = indices[i];
+      indices.splice(i, 1);
+      characters[index] = Characters[type][decs[index] % Characters[type].length];
+    });
+    return characters.join('');
   }
 };
 
--- test/unit/keymaker-toolbox.js
+++ test/unit/keymaker-toolbox.js
@@ -1,18 +1,13 @@
 import keymakerToolbox from '../../src/keymaker-toolbox';
 
 describe('keymakerToolbox', () => {
-  describe('Greet function', () => {
-    beforeEach(() => {
-      spy(keymakerToolbox, 'greet');
-      keymakerToolbox.greet();
+  describe('makeKey function', () => {
+    it('should return a key', () => {
+      expect(keymakerToolbox.makeKey('password', 'salt')).to.equal('$s2d$0TF2c6`k$2e$9PqySg`0PVs:BRJ');
     });
 
-    it('should have been run once', () => {
-      expect(keymakerToolbox.greet).to.have.been.calledOnce;
-    });
-
-    it('should have always returned hello', () => {
-      expect(keymakerToolbox.greet).to.have.always.returned('hello');
+    it('should return hex when all false', () => {
+      expect(keymakerToolbox.makeKey('password', 'salt', 100000, 32, false, false, false, false)).to.equal('0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5');
     });
   });
 });
